eeb8dc45326d2d0f910dec60b76b146535db92b1
Oliver Klee <typo3-coding@oliverklee.de>
2014-07-28 00:50:01 +0200

    [TASK] Switch to Selenium2TestCase.

--- Tests/Selenium/OliverKleeDeTest.php
+++ Tests/Selenium/OliverKleeDeTest.php
@@ -6,7 +6,7 @@ namespace OliverKleeDe\SeleniumDemo\Tests\Selenium;
  *
  * @author Oliver Klee <typo3-coding@oliverklee.de>
  */
-class OliverKleeDeTest extends \PHPUnit_Extensions_SeleniumTestCase {
+class OliverKleeDeTest extends \PHPUnit_Extensions_Selenium2TestCase {
 	protected function setUp() {
 		$this->setBrowser('firefox');
 		$this->setBrowserUrl('http://www.oliverklee.de/');
@@ -16,7 +16,10 @@ class OliverKleeDeTest extends \PHPUnit_Extensions_SeleniumTestCase {
 	 * @test
 	 */
 	public function homepageHasHomePageTitle() {
-		$this->open('http://www.oliverklee.de/');
-		$this->assertTitle('Startseite - oliverklee.de');
+		$this->url('http://www.oliverklee.de/');
+		$this->assertSame(
+			'Startseite - oliverklee.de',
+			$this->title()
+		);
 	}
 }
