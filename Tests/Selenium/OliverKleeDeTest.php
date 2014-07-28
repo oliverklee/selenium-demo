933a5bfc432e6f6a66d371f064c540129385ecdf
Oliver Klee <typo3-coding@oliverklee.de>
2014-07-28 15:34:25 +0200

    [TASK] Add demo methods for open and assertText.

--- Tests/Selenium/OliverKleeDeTest.php
+++ Tests/Selenium/OliverKleeDeTest.php
@@ -16,10 +16,38 @@ class OliverKleeDeTest extends AbstractSeleniumTestcase {
 	 * @test
 	 */
 	public function homepageHasHomePageTitle() {
-		$this->url('http://www.oliverklee.de/');
+		$this->open();
 		$this->assertSame(
 			'Startseite - oliverklee.de',
 			$this->title()
+		);
+	}
+
+	/**
+	 * @test
+	 *
+	 * "open" => "$this->url()"
+	 *
+	 * open /startseite.html
+	 */
+	public function open() {
+		$this->url('/startseite.html');
+	}
+
+	/**
+	 * @test
+	 *
+	 * "assertText" => "assertSame", vorher Element mit byCssSelector holen, dann ->text() auf dem Element
+	 *
+	 * assertText "css=#c1 > h1" "Dienstleistungen rund um Web-Entwicklung und Sicherheit sowie effektive Seminare, die Spaß machen"
+	 */
+	public function assertTextWithCssSelector() {
+		$this->open();
+		$element = $this->byCssSelector('#c1 > h1');
+
+		$this->assertSame(
+			'Dienstleistungen rund um Web-Entwicklung und Sicherheit sowie effektive Seminare, die Spaß machen',
+			$element->text()
 		);
 	}
 }
