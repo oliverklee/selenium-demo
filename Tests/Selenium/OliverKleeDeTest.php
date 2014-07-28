eea605c979644ec6e956d3a0602a324d73b4d16a
Oliver Klee <typo3-coding@oliverklee.de>
2014-07-28 15:48:25 +0200

    [TASK] Add click demos

--- Tests/Selenium/OliverKleeDeTest.php
+++ Tests/Selenium/OliverKleeDeTest.php
@@ -42,12 +42,44 @@ class OliverKleeDeTest extends AbstractSeleniumTestcase {
 	 * assertText "css=#c1 > h1" "Dienstleistungen rund um Web-Entwicklung und Sicherheit sowie effektive Seminare, die Spaß machen"
 	 */
 	public function assertTextWithCssSelector() {
-		$this->open();
+		$this->url('/startseite.html');
 		$element = $this->byCssSelector('#c1 > h1');
 
 		$this->assertSame(
 			'Dienstleistungen rund um Web-Entwicklung und Sicherheit sowie effektive Seminare, die Spaß machen',
 			$element->text()
+		);
+	}
+
+	/**
+	 * @test
+	 *
+	 * "clickAndWait" "link=Impressum" => byLinkText, click
+	 *
+	 * Wichtig: PHPUnit wartet automatisch, wenn das Klickziel ein echter Link ist und keine JavaScript-Sache.
+	 */
+	public function clickAndWaitWithLinkTarget() {
+		$this->url('/startseite.html');
+		$this->byLinkText('Impressum')->click();
+	}
+
+	/**
+	 * @test
+	 *
+	 * In Selenium IDE ist das 2x type und 1x clickAndWait.
+	 *
+	 * type -> value
+	 */
+	public function login() {
+		$this->url('http://typo3master.local/index.php?id=13');
+		$this->byId('user')->value('klee');
+		$this->byId('pass')->value('12345678');
+
+		$this->byName('submit')->click();
+
+		$this->assertSame(
+			'Login successful',
+			$this->byXPath('//*[@id="c3"]/div/h3')->text()
 		);
 	}
 }
