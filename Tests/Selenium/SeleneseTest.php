bc1f295b0e59ed05b9a4786bfed444ed6b18e235
Oliver Klee <typo3-coding@oliverklee.de>
2015-06-16 00:57:22 +0200

    [FEATURE] Add a Selenese demo

---
+++ Tests/Selenium/SeleneseTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace OliverKleeDe\SeleniumDemo\Tests\Selenium;
+
+/**
+ * Testcase.
+ *
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ */
+class SeleneseTest extends \PHPUnit_Extensions_SeleniumTestCase {
+	protected function setUp() {
+		$this->setBrowser('firefox');
+		$this->setBrowserUrl('https://www.oliverklee.de/');
+	}
+
+	/**
+	 * @test
+	 */
+	public function googleCanBeLoaded() {
+		$this->runSelenese(__DIR__ . '/Selenese/oliverklee.de.html');
+	}
+}
